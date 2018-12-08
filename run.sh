bb9d21b89397d972450b9f6d7f0788339b0857de
Peter Boin <Peter.Boin@gmail.com>
2018-12-08 17:18:25 +1100

    changed to a run/watch setup

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
-#!/bin/sh
-CompileDaemon -build 'echo' -command 'python serve.py' -pattern "\.py"
+#!/usr/bin/env bash
+
+# Create database
+db_file=data.db
+if [ ! -f "${db_file}" ] ; then
+    echo creating empty database: ${db_file}
+    sqlite3 ${db_file} ""
+fi
+
+# Run serve
+python3 serve.py
