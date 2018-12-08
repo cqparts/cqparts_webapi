4d5fc1e9f374a3151374edb9850d040b8ada4ca9
Peter Boin <Peter.Boin@gmail.com>
2018-12-08 21:58:31 +1100

    changed sqlite database to :memory:

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-# Create database
-db_file=data.db
-if [ ! -f "${db_file}" ] ; then
-    echo creating empty database: ${db_file}
-    sqlite3 ${db_file} ""
-fi
-
 # Run serve
 python3 serve.py
