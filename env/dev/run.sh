93124de5512b49f6c45a561279a2cb155f14e16f
Peter Boin <Peter.Boin@gmail.com>
2018-12-10 16:43:27 +1100

    ctl+c kills process

--- env/dev/run.sh
+++ env/dev/run.sh
@@ -4,10 +4,11 @@ source ./common-vars.sh
 port=8089
 
 case "$1" in
-    tests|"")
+    "")
         docker run --rm \
             --volume ${REPO_ROOT}:/code \
             --publish ${port}:${port} \
+            -it \
             ${IMAGE}
         ;;
     *)
