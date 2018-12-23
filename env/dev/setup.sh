a9c6dc719204e3d710f3eb7b1f897c51742797f9
Peter Boin <Peter.Boin@gmail.com>
2018-12-24 08:39:36 +1100

    install watchman currently affected by facebook/watchman#660

--- env/dev/setup.sh
+++ env/dev/setup.sh
@@ -10,11 +10,26 @@ Environment Variables:
 EOF
 
 # ------ Install Packages
+apt-get update
 # apt
-apt install sqlite3
+apt-get install sqlite3 -y
 # pip
 python3 -m pip install --upgrade pip
 python3 -m pip install -r /code/requirements.txt
+# watchman
+#   variation of: https://facebook.github.io/watchman/docs/install.html#installing-from-source
+apt-get install libssl-dev autoconf automake libtool pkg-config -y
+watchman_ver=4.9.0
+pushd /tmp
+wget -nv https://github.com/facebook/watchman/archive/v${watchman_ver}.tar.gz \
+    -O watchman-${watchman_ver}.tar.gz
+tar -xzf watchman-${watchman_ver}.tar.gz
+cd watchman-${watchman_ver}
+./autogen.sh
+./configure
+make
+make install
+popd
 
 # ------ Configure
 sqlite3 /code/data.db ""
