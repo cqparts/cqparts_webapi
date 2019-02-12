e740f87fcb52d9f910589c6a145e1711bcd2521d
Simon Kirkby <obeygiantrobot@gmail.com>
2019-02-12 12:37:10 +0800

    need to work on importotron

--- importer.py
+++ importer.py
@@ -1,26 +1,32 @@
 """
 
-Alternate importer for designs 
+Alternate importer for designs
 
 """
 
 from importlib import import_module
 import sys, os
 
+class meta:
+    pass
+
 sys.path.append("..")
 class importotron:
     def __init__(self,mods):
         self.mods = mods
+        base = meta()
         for i in self.mods:
             try:
                 m = import_module(i)
                 print("importing "+i)
+                setattr(base,i,m)
                 if hasattr(m,"__all__"):
                     l = m.__all__
                     for j in l:
-                        print(i,j)
+                        print(i,j,m)
             except:
                 print("fail on "+i)
+        self.base = base
 
 
 mods = [ 'borken' , 'cqparts_bucket' , 'experimental' ]
